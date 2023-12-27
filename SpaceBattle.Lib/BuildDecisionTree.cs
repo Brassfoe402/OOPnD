5823a17e65a1a7626557ac4d69d817064283717d
Brassfoe402 <arsenyi_serebrennikov@mail.ru>
2023-12-28 00:19:06 +0600

    add

--- SpaceBattle.Lib/BuildDecisionTree.cs
+++ SpaceBattle.Lib/BuildDecisionTree.cs
@@ -1,1 +1,28 @@
-﻿
+using Hwdtech;
+using Hwdtech.Ioc;
+
+namespace SpaceBattle.Lib;
+
+public class BuildDecisionTree : ICommand
+{
+    private IRead iread;
+    public BuildDecisionTree(IRead read)
+    {
+        iread = read;
+    }
+    public void Execute()
+    {
+        var vectors = iread.ReadFile();
+
+        var BuildTree = IoC.Resolve<Dictionary<int, object>>("Game.BuildDecisionTree");
+
+        vectors.ForEach(line => {
+            var DecisionTree = BuildTree;
+            line.ToList().ForEach(vector =>
+            {
+                DecisionTree.TryAdd(vector, new Dictionary<int, object>());
+                DecisionTree = (Dictionary<int, object>)DecisionTree[vector];
+            });
+        });
+    }
+}
